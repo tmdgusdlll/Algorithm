580fc330063ad74cda7d679461d815e98177ec76
SeungHyeonLee <leesh27955@gmail.com>
2026-07-14 19:45:14 +0900

    [level 0] Title: 이어 붙인 수, Time: 5.01 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] num_list) {
+        String even = "";
+        String odd = "";
+        
+        for (int num : num_list) {
+            if (num % 2 == 0) {
+                even += num;
+            } else {
+                odd += num;
+            }
+        }
+        return Integer.parseInt(even) + Integer.parseInt(odd);
+    }
+}
