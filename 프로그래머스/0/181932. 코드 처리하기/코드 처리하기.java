fa2ba43c9fcc9289074f763554d80817a0467623
SeungHyeonLee <leesh27955@gmail.com>
2026-07-14 18:43:23 +0900

    [level 0] Title: 코드 처리하기, Time: 5.16 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -6,29 +6,16 @@ class Solution {
         for (int i = 0; i < code.length(); i++) {
             char c = code.charAt(i);
             
-            // mode가 0일때
-            if (mode == 0) {
-                // idx가 1이면 mode = 1로 변경
-                if (c == '1') {
-                    mode = 1;
-                // idx가 짝수라면 ret에 추가
-                } else if (i % 2 == 0) {
-                    ret.append(c);
-                }
-            // mode가 1일때
+            if (c == '1') {
+                mode = 1 - mode;
             } else {
-                // idx가 1이면 mode = 0로 변경
-                if (c == '1') {
-                    mode = 0;
-                // idx가 홀수라면 ret에 추가
-                } else if (i % 2 != 0) {
+                if (mode == 0 && i % 2 == 0) {
+                    ret.append(c);
+                } else if (mode == 1 && i % 2 != 0) {
                     ret.append(c);
                 }
             }
         }
-        if (ret.length() == 0) {
-            return "EMPTY";
-        }
-        return ret.toString();
+        return ret.length() == 0 ? "EMPTY" : ret.toString();
     }
 }
